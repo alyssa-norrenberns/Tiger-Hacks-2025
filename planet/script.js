add51820af023b5511cfc287dd23ec780502195e
Carson Mensink <mensinkcarson@gmail.com>
2025-11-08 04:19:57 -0600

    added space background and planet 3d modeling

--- planet/script.js
+++ planet/script.js
@@ -8,3 +8,54 @@ backButton.addEventListener("click", () => {
 const hash = window.location.hash.substring(1); // Substring removes the #
 const titleElement = document.querySelector(".title");
 titleElement.textContent = hash.charAt(0).toUpperCase() + hash.slice(1);
+
+window.onload = () => {
+    const container = document.querySelector(".planet-container");
+    // Make scene for object
+    const scene = new THREE.Scene();
+    
+    // Create camera
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        container.clientWidth / container.clientHeight,
+        0.1,
+        1000
+    );
+    camera.position.z = 50;
+    
+    // Create renderer
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    container.appendChild(renderer.domElement);
+
+    // Create geometry + material with texture
+    const geometry = new THREE.SphereGeometry(15, 32, 32);
+    const texture = new THREE.TextureLoader().load(`../textures/${hash}.jpg`);
+    const material = new THREE.MeshPhongMaterial({ map: texture, shininess: 10 });
+    const sphere = new THREE.Mesh(geometry, material);
+    scene.add(sphere);
+
+    const light = new THREE.DirectionalLight(0xffffff, 1.5);
+    light.position.set(50, 50, 50);
+    scene.add(light);
+    scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+
+    // Animation loop (where the sphere is drawn)
+    function animate() {
+        requestAnimationFrame(animate);
+        
+        // Rotation of sphere by default
+        sphere.rotation.y += 0.005;
+        sphere.rotation.x += 0.001;
+        
+        renderer.render(scene, camera);
+    }
+    animate();
+    
+    // Handle window resize
+    window.addEventListener("resize", () => {
+        camera.aspect = container.clientWidth / container.clientHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(container.clientWidth, container.clientHeight);
+    });
+};
--- routes/test.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify, request
-from models.test import Test
-
-t = Test()
-test_bp = Blueprint("test", __name__)
-
-@test_bp.route("/", methods=["GET"])
-def test():
-    return jsonify({"message": t.id})
