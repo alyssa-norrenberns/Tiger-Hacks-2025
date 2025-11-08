b4fa047c56e6092fa2a2a7e2c6f7687e9dc64a0b
apaten0403 <149838801+apaten0403@users.noreply.github.com>
2025-11-08 09:41:14 +0000

    Added information about Mercury

---
+++ models/mercury.py
@@ -0,0 +1,30 @@
+class Mercury:
+    def __init__(self):
+        self.name = 'Mercury'          # Store the name in the object
+        self.atmosphere = 4879            #km above Mercury's surface (traces of Na, K, He, H, O)
+        self.mass = 3.30114 * 10^23
+        self.volume = 6.083 * 10^10
+        self.density = 5.4291
+        self.gravity = 3.7
+        self.temp = 440
+        self.sideral_orbit = 87.969
+        self.sideral_rotation = 1407.6
+        self.moons = 0
+        self.radius = 2440              #radius in km
+        
+        self.core = '70%'            #% of Mercury that core takes up
+        self.core_FE = '60%'           #% of Core that Iron takes up
+        self.core_NI = '4%'            #% of Core that Nickel takes up  
+        self.core_S = '4%'              #% of Core that Sulfer takes up 
+        self.core_Si = '4%'             #% of Core that Silicon takes up 
+        
+        self.mantle = '30%'          #% of Mercury that mantle takes up
+        self.mantle_MgO = '22%'      #% of mantle that MgO takes up
+        self.mantle_SiO = '25%'        #% of mantle that CaO takes up  
+        self.mantle_CaO = '5%'         #% of mantle that CaO takes up
+        self.mantle_AlO = '5%'          #% of mantle that AlO takes up
+        self.mantle_FeO =  '3%'          #% of mantle that FeO takes up
+        
+
+        
+        
